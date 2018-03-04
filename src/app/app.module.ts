689eb44de3805e9a27084ca663e5f79ca72075a3
intellectape <adi_b09@hotmail.com>
2018-03-04 03:49:50 -0500

    Updating the whole repository with new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectDetailComponent
   ],
   imports: [
     BrowserModule
